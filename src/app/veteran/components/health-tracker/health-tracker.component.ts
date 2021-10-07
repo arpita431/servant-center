6b97bb15262d7e42f7b8da8836da627ae7ec1208
Arpita Maurya <Arpita.Maurya@mindtree.com>
2021-10-07 13:01:39 +0530

    format

--- src/app/veteran/components/health-tracker/health-tracker.component.ts
+++ src/app/veteran/components/health-tracker/health-tracker.component.ts
@@ -3,10 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-health-tracker',
   templateUrl: './health-tracker.component.html',
-  styleUrls: ['./health-tracker.component.scss'],
+  styleUrls: ['./health-tracker.component.scss']
 })
 export class HealthTrackerComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
