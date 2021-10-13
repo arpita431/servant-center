dfb44ce0ef3828f23f52de76f670525ab9cc4580
Arpita Maurya <Arpita.Maurya@mindtree.com>
2021-10-13 10:05:41 +0530

    mobile view issue fixed

--- src/app/case-worker/components/case-worker-profile/case-worker-profile.component.ts
+++ src/app/case-worker/components/case-worker-profile/case-worker-profile.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CaseWorkerProfileComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log("case worker profile component")
+  }
 }
